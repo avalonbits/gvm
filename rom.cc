2d6e97c50714d670340f91b12c678868e0564d53
Igor Cananea <icc@avalonbits.com>
2018-12-30 17:27:34 -0800

    Have rom be read from and written to file.

--- rom.cc
+++ rom.cc
@@ -4,6 +4,29 @@
 
 namespace gvm {
 
+/* static */
+const Rom* Rom::FromFile(std::ifstream& in) {
+  const std::string header("1987gvm");
+
+  char hBuffer[7];
+  in.read(hBuffer, 7);
+  assert(header.compare(0, 7, hBuffer, 7) == 0);
+
+  auto* rom = new Rom();
+  while (true) {
+    uint32_t addr;
+    in.read(reinterpret_cast<char*>(&addr), 4);
+    uint32_t wCount;
+    in.read(reinterpret_cast<char*>(&wCount), 4);
+    assert(wCount <= (256 << 20));
+    uint32_t* buffer = new uint32_t[wCount];
+    in.read(reinterpret_cast<char*>(buffer), wCount*4);
+    rom->Load(addr, buffer, wCount);
+  }
+
+  return rom;
+}
+
 Rom::Rom(uint32_t memaddr, const std::vector<Word>& words) {
   Load(memaddr, words);
 }
@@ -12,6 +35,26 @@ void Rom::Load(uint32_t memaddr, const std::vector<Word>&  words) {
   assert(!words.empty());
   assert(memaddr % kWordSize == 0);
   rom_[memaddr] = words;
+}
+
+void Rom::Load(uint32_t memaddr, const uint32_t* words, uint32_t size) {
+  std::vector<Word>  program(words, words + size);
+  Load(memaddr, program);
+}
+
+void Rom::ToFile(std::ofstream& out) const {
+  std::vector<uint32_t> buffer;
+
+  for (const auto& kv : rom_) {
+    buffer.push_back(kv.first);
+    buffer.push_back(kv.second.size());
+    buffer.insert(buffer.end(), kv.second.begin(), kv.second.end());
+  }
+
+  out.write("1987gvm", 7);
+  out.write(reinterpret_cast<const char*>(buffer.data()),
+            buffer.size() * sizeof(uint32_t));
+  out.close();
 }
 
 }  // namespace gvm
--- rom.h
+++ rom.h
@@ -1,23 +1,29 @@
 #ifndef _GVM_ROM_H_
 #define _GVM_ROM_H_
 
-#include <vector>
+#include <fstream>
 #include <map>
+#include <vector>
 
 #include "isa.h"
 
 namespace gvm {
 class Rom {
  public:
+  static const Rom* FromFile(std::ifstream& in);
+
   Rom(uint32_t memaddr, const std::vector<Word>& words);
   Rom(const Rom&) = delete;
   Rom& operator=(const Rom&) = delete;
   virtual ~Rom() {}
 
   void Load(uint32_t memaddr, const std::vector<Word>& words);
+  void Load(uint32_t memaddr, const uint32_t* words, uint32_t size);
   const std::map<uint32_t, std::vector<Word>>& Contents() const { return rom_; }
+  void ToFile(std::ofstream& out) const;
 
  private:
+  Rom() {};
   std::map<uint32_t, std::vector<Word>> rom_;
 };
 }  // namespace gvm
