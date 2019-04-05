2483c30ad144d233625631368c5fe298da5a222f
Igor Cananea <icc@avalonbits.com>
2019-04-05 12:40:52 -0700

    Fixed paths for run files.

--- run_bench.sh
+++ run_bench.sh
@@ -1,1 +1,1 @@
-killall -9 gvm; cd /home/ronaldo/gvm/gsm && go install && cd .. && gsm -o bench.rom benchmark.asm && scons -j6   && ./gvm --prgrom=bench.rom --video_mode=null
+killall -9 gvm; cd $HOME/gvm/gsm && go install && cd .. && gsm -o bench.rom benchmark.asm && scons -j6   && ./gvm --prgrom=bench.rom --video_mode=null
