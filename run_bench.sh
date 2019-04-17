7af825e8c02178a5414e054ef808ecd57396887a
Igor Cananea <icc@avalonbits.com>
2019-04-16 22:44:25 -0700

    Update run_kernel to allow video mode selection.
    While in the neighborhood, updated run_bench to use new syntax for default values.

--- run_bench.sh
+++ run_bench.sh
@@ -1,2 +1,2 @@
-BENCH=${BENCH:-benchmark.asm}
+: "${BENCH:=benchmark.asm}"
 killall -9 gvm; cd $HOME/gvm/gsm && go install && cd .. && gsm -o bench.rom $BENCH && scons -j6   && ./gvm --prgrom=bench.rom --video_mode=null
--- run_kernel.sh
+++ run_kernel.sh
@@ -1,3 +1,4 @@
-echo "killall -9 gvm; cd $HOME/gvm/gsm && go install && cd .. && gsm -o kernel.rom kernel.asm && scons -j6   && ./gvm --prgrom=kernel.rom --video_mode=720p"
+: "${VMODE:=720p}"
+echo "killall -9 gvm; cd $HOME/gvm/gsm && go install && cd .. && gsm -o kernel.rom kernel.asm && scons -j6   && ./gvm --prgrom=kernel.rom --video_mode=$VMODE"
 
-killall -9 gvm; cd $HOME/gvm/gsm && go install && cd .. && gsm -o kernel.rom kernel.asm && scons -j6   && ./gvm --prgrom=kernel.rom --video_mode=720p
+killall -9 gvm; cd $HOME/gvm/gsm && go install && cd .. && gsm -o kernel.rom kernel.asm && scons -j6   && ./gvm --prgrom=kernel.rom --video_mode=$VMODE
