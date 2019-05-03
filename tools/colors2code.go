ffd88bd24fd1ab8c39ab3a4980185e51b0cb3b04
Igor Cananea <icc@avalonbits.com>
2019-05-03 15:45:22 -0700

    Updated colors2code to output a c array and added the color table to sdl2 video display.

--- tools/colors2code.go
+++ tools/colors2code.go
@@ -10,20 +10,29 @@ import (
 	"strings"
 )
 
+var (
+	asm = flag.Bool("asm", false, "If true, outputs gvm assembly with a color table.")
+	cc  = flag.Bool("cc", false, "if true, outputs a c static array with the color table.")
+)
+
 func main() {
 	flag.Parse()
+
+	if !*asm && !*cc {
+		panic("Pick -asm or -cc")
+	}
 	inJson, err := os.Open(flag.Arg(0))
 	if err != nil {
 		panic(err)
 	}
 	defer inJson.Close()
 
-	outAsm, err := os.Create(flag.Arg(1))
+	outCode, err := os.Create(flag.Arg(1))
 	if err != nil {
 		panic(err)
 	}
-	defer outAsm.Close()
-	out := bufio.NewWriter(outAsm)
+	defer outCode.Close()
+	out := bufio.NewWriter(outCode)
 	defer out.Flush()
 
 	dec := json.NewDecoder(inJson)
@@ -31,9 +40,28 @@ func main() {
 	if err := dec.Decode(&cs); err != nil && err != io.EOF {
 		panic(err)
 	}
-	for _, c := range cs {
+	for i := range cs {
+		c := &cs[i]
 		c.HexString = "0xFF" + strings.ToUpper(reverse(c.HexString)[:len(c.HexString)-1])
-		out.WriteString(fmt.Sprintf("\t.int %s ; %3d - %s\n", c.HexString, c.Id, c.Name))
+	}
+	if *asm {
+		for _, c := range cs {
+			out.WriteString(fmt.Sprintf("\t.int %s ; %3d - %s\n", c.HexString, c.Id, c.Name))
+		}
+	} else if *cc {
+		out.WriteString("static const uint32_t kColorTable[256] = {")
+		for i, c := range cs {
+			if i%8 == 0 {
+				if i != 0 {
+					out.WriteString(", ")
+				}
+				out.WriteString("\n\t")
+			} else {
+				out.WriteString(", ")
+			}
+			out.WriteString(c.HexString)
+		}
+		out.WriteString("\n};")
 	}
 }
 
