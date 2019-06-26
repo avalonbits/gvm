f1abdf902f0a9da4dd9bbfb060040ccccc63c99c
Igor Cananea <icc@avalonbits.com>
2019-06-25 20:43:12 -0700

    Changed the default video mode to 450p to match text mode.

--- run_kernel.sh
+++ run_kernel.sh
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-: "${VMODE:=900p}"
+: "${VMODE:=450p}"
 : "${CPU:=0}"
 : "${DISK_FILE:=/tmp/gvm.hd}"
 : "${DSTEP:=0}"
