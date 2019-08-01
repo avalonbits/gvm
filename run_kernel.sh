f5f09e68e9d41e180189e4cd8c54a3fc29740a3a
Igor Cananea <icc@avalonbits.com>
2019-07-31 22:55:16 -0700

    Updated run_kernel.sh to allow any kernel to be run.

--- run_kernel.sh
+++ run_kernel.sh
@@ -20,7 +20,8 @@
 : "${CPU:=0}"
 : "${DISK_FILE:=/tmp/gvm.hd}"
 : "${DSTEP:=0}"
-CMD="killall -9 gvm; cd $HOME/gvm/gsm && go install && cd .. && gsm -o kernel.rom kernel.asm && scons -j6 dstep=$DSTEP  && ./gvm --prgrom=kernel.rom --video_mode=$VMODE --disk_file=$DISK_FILE"
+: "${KERNEL:=kernel.asm}"
+CMD="killall -9 gvm; cd $HOME/gvm/gsm && go install && cd .. && gsm -o kernel.rom $KERNEL && scons -j6 dstep=$DSTEP  && ./gvm --prgrom=kernel.rom --video_mode=$VMODE --disk_file=$DISK_FILE"
 
 echo $CMD
 eval $CMD
